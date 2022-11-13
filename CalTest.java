96090fa2520e0d62f6c708089a6ad4c73b2c10e8
CITANA-CCCLXV <114452630+CITANA-CCCLXV@users.noreply.github.com>
2022-11-12 21:55:12 -0500

    Add files via upload

---
+++ CalTest.java
@@ -0,0 +1,55 @@
+package assignment2SWE637;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+public class CalTest {
+
+	@RunWith (Parameterized.class)
+	public class ParameterizedCalTest
+	{ 
+		int month1; 
+		int day1; 
+		int month2;
+		int day2;
+		int year;
+		int numDays; 
+
+
+
+		//inject values via constructor
+		public ParameterizedCalTest (int month1, int day1, int month2, int day2, int year, int numDays) {
+			this.month1 = month1; 
+			this.day1 = day1;
+			this.month2 = month2;
+			this.day2 = day2;
+			this.year = year;
+			this.numDays = numDays;
+		}
+
+
+
+
+		@Parameters 
+		public Collection <Object[]> data(){
+			return Arrays.asList (new Object [][]{
+				{1,2,1,3,2014}
+
+			});
+		}
+
+		@Test //creates a test method
+		public void test_Caldate() {
+			assertEquals(Cal.cal(month1, day1, month2, day2, year));
+			
+			
+		}//END CalTest CLASS
+		
+
+		}
+}
